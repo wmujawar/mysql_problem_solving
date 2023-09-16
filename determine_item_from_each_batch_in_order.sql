7535923ee7822885469a07d5247272f934e7cd18
Wasim Mujawar <mws55@ymail.com>
2023-09-16 20:34:30 +0530

    New query to determine items ordered from which batch

---
+++ determine_item_from_each_batch_in_order.sql
@@ -0,0 +1,69 @@
+use practice;
+
+drop table if exists batch;
+drop table if exists orders;
+
+create table batch (batch_id varchar(10), quantity integer);
+create table orders (order_number varchar(10), quantity integer);
+
+
+insert into batch values ('B1', 5);
+insert into batch values ('B2', 12);
+insert into batch values ('B3', 8);
+
+insert into orders values ('O1', 2);
+insert into orders values ('O2', 8);
+insert into orders values ('O3', 2);
+insert into orders values ('O4', 5);
+insert into orders values ('O5', 9);
+insert into orders values ('O6', 5);
+
+select * from batch;
+select * from orders;
+
+WITH batch_cte AS
+(
+	SELECT *, ROW_NUMBER() OVER(ORDER BY batch_id) AS rn
+	FROM (
+		WITH RECURSIVE batch_split AS (
+			SELECT batch_id, 1 as quantity FROM batch
+			
+			UNION ALL
+			
+			SELECT bs.batch_id, (bs.quantity + 1) AS quantity
+			FROM batch_split bs
+			INNER JOIN batch b
+			ON bs.batch_id = b.batch_id AND b.quantity > bs.quantity
+		)
+
+		SELECT batch_id, 1 AS quantity
+		FROM batch_split
+	) batch_details
+),
+
+order_cte AS
+(
+	SELECT *, ROW_NUMBER() OVER(ORDER BY order_number) AS rn
+	FROM (
+		WITH RECURSIVE order_split AS (
+			SELECT order_number, 1 AS quantity FROM orders
+			
+			UNION ALL
+			
+			SELECT os.order_number, (os.quantity + 1) AS quantity
+			FROM order_split os
+			INNER JOIN orders o
+			ON os.order_number = o.order_number AND o.quantity > os.quantity
+		)
+
+		SELECT order_number, 1 AS quantity
+		FROM order_split
+	) order_details
+)
+
+SELECT order_number, batch_id, SUM(o.quantity) AS quantity
+FROM order_cte o
+LEFT OUTER JOIN batch_cte b
+ON o.rn = b.rn
+GROUP BY 1, 2
+ORDER BY order_number, batch_id;
